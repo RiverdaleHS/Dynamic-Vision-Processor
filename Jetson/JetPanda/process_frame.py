c128f74534507a6d89f084e2eda579ed2246f8a9
Henry O'Scannlain-Miller <henryom@me.com>
2017-07-31 10:17:37 -0700

    structured this file for Andrew

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -1,6 +1,6 @@
 import cv2
 
-def process_frame(frame, vars):
+def process_frame(frame, low_range, high_range, target):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     binary = cv2.inRange(hsv, (10, 10, 10), (200, 200, 200))
     random_image_why_is_this_here, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
