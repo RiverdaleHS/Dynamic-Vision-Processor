e88394ad3f37b031b9664eb9f8bd48a9fb415b3b
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-01 13:21:34 -0700

    added morhp again

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -9,10 +9,9 @@ def process_frame(frame, low_range, high_range, target):
     # Value 0-255
 
     binary = cv2.inRange(hsv, low_range, high_range)
-    morph_binary = binary
     # clean up binary frame
     kernal = np.ones((16, 16), np.uint8)
-    morph_binary = cv2.dilate(morph_binary, kernal, iterations=1)
+    morph_binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernal)
 
 
     #_, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
