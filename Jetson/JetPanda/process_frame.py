ef5b5bd2360dc58ec5e3acf2da6248771912f8fd
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-01 09:55:45 -0700

    fixing contours

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -8,7 +8,7 @@ def process_frame(frame, low_range, high_range, target):
     # Value 0-255
 
     binary = cv2.inRange(hsv, low_range, high_range)
-    # random_image_why_is_this_here, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(frame, -1, (0, 255, 0), 3) THIS IS CAUSING PROBLEMS
-    cv2.imshow("raw", frame)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), -1)
+    cv2.imshow("frame", frame)
     cv2.imshow("binary", binary)
