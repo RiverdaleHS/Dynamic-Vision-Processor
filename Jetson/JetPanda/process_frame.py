b8c07cefd64e04f791aa3f8f9cc6e6e24afb25ae
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-01 12:41:22 -0700

    added dilate

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def process_frame(frame, low_range, high_range, target):
@@ -8,6 +9,11 @@ def process_frame(frame, low_range, high_range, target):
     # Value 0-255
 
     binary = cv2.inRange(hsv, low_range, high_range)
+    #dialate image
+    kernal = np.ones((5,5), np.uint8)
+    cv2.dilate(binary, kernal, iterations=1)
+
+
     _, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(frame, contours, -1, (255, 140, 0), -1)
     cv2.imshow("frame", frame)
