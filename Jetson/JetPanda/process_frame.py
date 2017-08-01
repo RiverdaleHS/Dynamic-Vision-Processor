7fd59ddd171d7968df6159ed8770817a51a850c4
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-01 11:36:30 -0700

    changed target highlighting color

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -9,6 +9,6 @@ def process_frame(frame, low_range, high_range, target):
 
     binary = cv2.inRange(hsv, low_range, high_range)
     _, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame, contours, -1, (0, 255, 0), -1)
+    cv2.drawContours(frame, contours, -1, (0, 140, 255), -1)
     cv2.imshow("frame", frame)
     cv2.imshow("binary", binary)
