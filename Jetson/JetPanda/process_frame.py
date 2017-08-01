9abffe34021e40e940ef769516b9f8165d31af7f
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-01 13:30:14 -0700

    lit meme

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -12,11 +12,12 @@ def process_frame(frame, low_range, high_range, target):
     # clean up binary frame
     erode_kernal = np.ones((4, 4), np.uint8)
     dilate_kernal = np.ones((4, 4), np.uint8)
-    morph_binary = cv2.erode(binary, erode_kernal, iterations=1)
-    morph_binary = cv2.dilate(binary, dilate_kernal, iterations=1)
+    erode_binary = cv2.erode(binary, erode_kernal, iterations=1)
+    open_binary = cv2.dilate(binary, dilate_kernal, iterations=1)
 
     #_, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #cv2.drawContours(frame, contours, -1, (255, 140, 0), -1)
     cv2.imshow("frame", frame)
     cv2.imshow("binary", binary)
-    cv2.imshow("morphed binary", morph_binary)
+    cv2.imshow("erode binary", erode_binary)
+    cv2.imshow("open binary", open_binary)
