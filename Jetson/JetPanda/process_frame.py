809e92d510f14ea86a586cf404885b1e35993689
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-01 12:45:48 -0700

    made it easier to dev

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -9,12 +9,14 @@ def process_frame(frame, low_range, high_range, target):
     # Value 0-255
 
     binary = cv2.inRange(hsv, low_range, high_range)
+    morph_binary = binary
     # clean up binary frame
     kernal = np.ones((5,5), np.uint8)
-    cv2.morphologyEx(frame, cv2.MORPH_CLOSE, kernal)
+    cv2.morphologyEx(morph_binary, cv2.MORPH_CLOSE, kernal)
 
 
     #_, contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #cv2.drawContours(frame, contours, -1, (255, 140, 0), -1)
     cv2.imshow("frame", frame)
     cv2.imshow("binary", binary)
+    cv2.imshow("morphed binary", morph_binary)
