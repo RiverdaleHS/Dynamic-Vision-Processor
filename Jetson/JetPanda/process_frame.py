4d0e03c044e5ee0cb20a3ede5c531264b0debf13
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-02 11:20:28 -0700

    this is probably not gonna run but lets see

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -18,20 +18,39 @@ def process_frame(frame, low_range, high_range, targets):
     _, contours, hierarchy = cv2.findContours(open_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     filtered_contours = []
-    # Find Targets
+    # Remove crappy targets
     for contour in contours:
         # Eliminate small contours based on a fit line.  All targets should be level to the ground
         [vx, vy, x, y] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0, 0.01, 0.01)
-        if math.degrees(math.atan2(vy - y, vx - x)) < 90:
+        area = math.degrees(math.atan2(vy - y, vx - x))
+        if math.degrees(math.atan2(vy - y, vx - x)) < -150:
             filtered_contours.append(contour)
+
 
     for filtered_contour in filtered_contours:
         for target in targets:
-            area = cv2.contourArea(contour)
+            matches = []
             perimeter = cv2.arcLength(contour, True)
+            area = cv2.contourArea(contour)
+            perimeter_over_area = perimeter/area
+            # 0.55 - 0.5 = 0.05
+            if (perimeter_over_area - target.perimeter_over_area) < target.perimeter_over_area_tolerence:
+                matches.append(target)
+
+            if target.find_one:
+                # determine the best of the found targets
+                target.add_contours([matches[0]])
+            else:
+                target.add_contours(matches)
+
+
 
     cv2.drawContours(frame, contours, -1, (0, 0, 255), -1)
     cv2.drawContours(frame, filtered_contours, -1, (255, 0, 0), -1)
+    for target in target:
+        cv2.drawContours(frame, target.contours, -1, (0, 0, 255), -1)
+
+
     cv2.imshow("frame", frame)
     cv2.imshow("binary", binary)
     cv2.imshow("erode binary", erode_binary)
