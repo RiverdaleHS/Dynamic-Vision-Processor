ed7cd25ce6e93d67bed91a8e325a87be2397a23d
Henry O'Scannlain-Miller <henryom@me.com>
2017-08-03 13:33:04 -0700

    i am so damn stupid

--- Jetson/JetPanda/process_frame.py
+++ Jetson/JetPanda/process_frame.py
@@ -14,7 +14,7 @@ def process_frame(frame, low_range, high_range, targets):
     erode_kernal = np.ones((4, 4), np.uint8)
     dilate_kernal = np.ones((4, 4), np.uint8)
     erode_binary = cv2.erode(binary, erode_kernal, iterations=1)
-    open_binary = cv2.dilate(binary, dilate_kernal, iterations=1)
+    open_binary = cv2.dilate(erode_binary, dilate_kernal, iterations=1)
 
     _, contours, hierarchy = cv2.findContours(open_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -62,3 +62,4 @@ def process_frame(frame, low_range, high_range, targets):
     cv2.imshow("frame", frame)
     cv2.imshow("binary", binary)
     cv2.imshow("erode binary", erode_binary)
+    cv2.imshow("open binary", open_binary)
